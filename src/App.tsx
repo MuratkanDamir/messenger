3a24a701441f330f80f772c2bfd2f2c510064f75
MuratkanDamir <muratkandamir00@gmail.com>
2023-11-29 15:36:55 +0500

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-
+import {Routes, Route} from "react-router-dom";
 const App: React.FC = () => {
   return (
     <div >
-
+      <Routes>
+        <Route path="/" element={ <HomePage /> }/>
+        <Route path="/login" element={ <LoginPage /> }/>
+        <Route path="/register" element={ <RegisterPage /> }/>
+      </Routes>
     </div>
   );
 }
