8ff995621770ba169f600f462a40ced501e6ea18
MuratkanDamir <muratkandamir00@gmail.com>
2023-11-29 15:30:05 +0500

    add firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAe8fIIJWGHy6HOfrfDkCFXYFNmjwdS-5c",
+  authDomain: "messenger-996b8.firebaseapp.com",
+  projectId: "messenger-996b8",
+  storageBucket: "messenger-996b8.appspot.com",
+  messagingSenderId: "659350903082",
+  appId: "1:659350903082:web:60956d4ea59ad3d70f2653"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
