4e1d3f76469262cae9313dd718dc7f1f5448340a
MuratkanDamir <muratkandamir00@gmail.com>
2023-12-07 18:43:34 +0500

    complete chats output

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -2,60 +2,51 @@ import {useState, useEffect} from "react";
 import Avatar from '@mui/material/Avatar';
 
 import { db } from "firebaseApp";
-import { doc, addDoc, collection, getDocs, query , where, onSnapshot} from "firebase/firestore";
+import { doc, addDoc, collection, getDocs, query , where,onSnapshot} from "firebase/firestore";
 import { useAppDispatch, useAppSelector } from "hooks/hooks";
 
 interface Chat{
     user1: string,
+    user2: string,
 }
 
 const Chats: React.FC = () => {
     const [chats, setChats] = useState<Chat[]>([])
     const { id } = useAppSelector(state => state.user)
     const getChats = async () =>{
+        const q = query(collection(db, "chats"), 
+            where("user1", "==", id) ||
+            where("user2", "==", id)
+        );
 
-
-        const q = query(collection(db, "chats"), where("user1", "==", id), where("user2", "==", id)  );
-        // console.log(q);
-        onSnapshot(q, (querySnapshot) => {
-            const tempChats: Chat[] = [];
-            querySnapshot.forEach((doc) => {
-                const userChat: Chat = doc.data() as Chat;
-                console.log(userChat);
-                tempChats.push(userChat);
+        try{
+            await onSnapshot(q, (querySnapshot) => {
+                const tempChats: Chat[] = [];
+                querySnapshot.forEach((doc) => {
+                    const userChat: Chat = doc.data() as Chat;
+                    
+                    tempChats.push(userChat);
+                });
+                setChats(tempChats);
             });
-            setChats(tempChats);
-        });
+        } catch (error) {
+            console.error("Ошибка при выполнении запроса:", error);
+        }
+
     }
 
     useEffect(() => {
-        const fetchData = async () => {
-            const q = query(collection(db, "chats"), where("user1", "==", id));
-            const querySnapshot = await getDocs(q);
-            
-            const tempChats: Chat[] = [];
-            querySnapshot.forEach((doc) => {
-              const userChat: Chat = doc.data() as Chat;
-              tempChats.push(userChat);
-            });
-            console.log(tempChats);
-            setChats(tempChats);
-          };
-      
-        fetchData(); // Вызываем fetchData при создании компонента
-
-        // getChats();
-
-    }, []);
+        getChats();
+    }, [db]);
 
     return (
-        <div>
+        <div style={{display:'flex', flexDirection:'column' , gap:'15px', padding:'10px'}}>
             Chats
             {
-                chats.map((user)=>(
-                    <div style = {{display:'flex', alignItems:'center', gap:'5px'}}>
+                chats.map((user, index)=>(
+                    <div key={index} style = {{display:'flex', alignItems:'center', gap:'5px'}}>
                         <Avatar>H</Avatar>
-                        {user.user1}
+                        {user.user1 == id? user.user2: user.user1}
                     </div>
                 ))
             }
